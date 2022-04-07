922bbcb2690db4abe944b389f38b7a57e86d3485
juanpe <juancoiro17@gmail.com>
2022-04-06 21:37:07 -0300

    INCORPORANDO HTML

---
+++ java.js
@@ -0,0 +1,8 @@
+console.log("Incorporando HTML");
+let entrada = prompt("Te gusta el mate?");
+let entradaMayus = entrada.toUpperCase();
+if (entradaMayus == "SI"){
+    alert("CRACK");
+} else {
+    alert("UFF");
+}
