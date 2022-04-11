d5e0286efed63c7e5347868a372e008c8f482deb
juanpe <juancoiro17@gmail.com>
2022-04-11 19:08:42 -0300

    Eventos

--- java.js
+++ java.js
@@ -1,8 +1,4 @@
-console.log("Incorporando HTML");
-let entrada = prompt("Te gusta el mate?");
-let entradaMayus = entrada.toUpperCase();
-if (entradaMayus == "SI"){
-    alert("CRACK");
-} else {
-    alert("UFF");
-}
+let input1 = document.getElementById("nombre");
+let input2 = document.getElementById("edad");
+input1.onkeyup = () => {console.log("keyUp")};
+input2.onekeydown = () => {console.log("keyDown")};
