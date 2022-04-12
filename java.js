cea2a374789d369b98117f93200d85681d2bfc19
juanpe <juancoiro17@gmail.com>
2022-04-12 08:15:14 -0300

    Desafio de Eventos

--- java.js
+++
@@ -1,4 +0,0 @@
-let input1 = document.getElementById("nombre");
-let input2 = document.getElementById("edad");
-input1.onkeyup = () => {console.log("keyUp")};
-input2.onekeydown = () => {console.log("keyDown")};
