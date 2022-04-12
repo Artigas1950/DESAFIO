e8479240d36725b3b22a598021d8776a13e57aab
Juan Pedro Coiro <98821221+Artigas1950@users.noreply.github.com>
2022-04-12 20:25:32 -0300

    Delete Entrega1 directory

--- Entrega1/java.js
+++
@@ -1,8 +0,0 @@
-console.log("Incorporando HTML");
-let entrada = prompt("Te gusta el mate?");
-let entradaMayus = entrada.toUpperCase();
-if (entradaMayus == "SI"){
-    alert("CRACK");
-} else {
-    alert("UFF");
-}
